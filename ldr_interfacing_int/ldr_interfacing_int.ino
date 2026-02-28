0f41bc99415f0a2dfbcb4a4877fcf29cd1910899
Anna Melqonanyan <melqonanyanna@gmail.com>
2026-03-01 00:43:21 +0400

    added potentiometer for volume control

--- ldr_interfacing_int/ldr_interfacing_int.ino
+++ ldr_interfacing_int/ldr_interfacing_int.ino
@@ -1,11 +1,12 @@
 #define ldr_qty 3
-#define fet_gate 9
+#define fet_gate 5
+#define pot_out A4
 
-// const int ldr_pins[] = {2, 3, 4, 5, 6, 7, 8};
-const int ldr_pins[] = {2, 3, 8};
-const int planet_sounds[] = {262, 294, 330};
-// const int planet_sounds[] = {262, 294, 330, 349, 392, 440, 494};
-volatile int active_ldr = -1;
+int pot_val = 0;
+int volume = 0;
+const int ldr_pins[] = {2, 3, 8, 8, 12, 13, 16, 17};
+const int planet_sounds[] = {262, 294, 330, 349, 392, 440, 494};
+volatile int active_ldr = -1; // 0 is an index as well so we set to safe negative value
 
 void handler()
 {
@@ -24,19 +25,34 @@ void setup()
 {
   Serial.begin(9600);
   pinMode(fet_gate, OUTPUT);
+  pinMode(pot_out, INPUT);
   for (int i = 0; i < ldr_qty; i++)
   {
   	pinMode(ldr_pins[i], INPUT);
     attachInterrupt(digitalPinToInterrupt(ldr_pins[i]), handler, CHANGE);
   }
+  
 }
 
 
 void loop()
-{
-  if (active_ldr >= 0)
-    tone(fet_gate, planet_sounds[active_ldr]);
-  else
-    noTone(fet_gate);
-  delay(100);
+{  
+  pot_val = analogRead(pot_out);
+  Serial.print("Potentiometer ");
+  Serial.println(pot_val);
+
+  // volume = map(pot_val, 0, 1023, 10, 255);
+  volume = pow((float)pot_val / 1023.0, 2) * 255;
+  Serial.print("Mapped Volume ");
+  Serial.println(volume);
+  if (active_ldr >= 0) 
+  {
+    analogWrite(fet_gate, volume); 
+    delayMicroseconds(1000000 / (2 * planet_sounds[active_ldr]));
+  }
+  else 
+  {
+    analogWrite(fet_gate, 0);
+    delayMicroseconds(1000000 / (2 * planet_sounds[active_ldr]));
+  }
 }
